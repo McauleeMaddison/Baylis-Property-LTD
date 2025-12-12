b84cc7fd037756b71e2966b54322a5be781324c5
Mcaulee Maddison <vuz9wv37@students.codeinstitute.net>
2025-12-12 14:35:05 +0000

    Use env vars for MySQL connection

--- server/mysql.js
+++ server/mysql.js
@@ -1,10 +1,15 @@
-import mysql from 'mysql2/promise';
-import dotenv from 'dotenv';
+import mysql from "mysql2/promise";
+import dotenv from "dotenv";
+
 dotenv.config();
 
-export const db = await mysql.createConnection({
+const connectionConfig = {
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE
-});
+  database: process.env.MYSQL_DATABASE,
+};
+
+export const db = await mysql.createConnection(connectionConfig);
+
+console.log("Connected to MySQL database");
